f899bc81eaef00b2d21a79bae9815b0083144e03
grant smith <grantsmith1256@gmail.com>
2018-10-04 18:14:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase =="I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
-   elsif phrase ==
    end
+  if phrase == "Hi Nana, how are you?"
+    return ""
  end
