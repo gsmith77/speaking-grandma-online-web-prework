6a9c22dcb35ec43ccbc1493f67ca3247d4bb4575
grant smith <grantsmith1256@gmail.com>
2018-10-04 18:10:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
-def speak_to_grandma(phrase="I LOVE YOU GRANDMA!")
-  if "phrase!"=="I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase =="I LOVE YOU GRANDMA!"
      return "I LOVE YOU PUMPKIN!"
    end
  end
